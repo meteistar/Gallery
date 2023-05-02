bbff72c70c76167c8c963da404e9e0fcd2a94aea
meteistar <gargamel_1987@hotmail.com>
2023-05-02 17:23:09 +0300

    Added init() to pictureDao

--- Gallery-Core/picturedao.cpp
+++ Gallery-Core/picturedao.cpp
@@ -1,6 +1,24 @@
 #include "picturedao.h"
 
-PictureDao::PictureDao()
+#include <QStringList>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+
+PictureDao::PictureDao(QSqlDatabase& database) :
+    mDatabase(database)
 {
 
+}
+
+void PictureDao::init() const
+{
+    QStringList list = mDatabase.tables();
+
+    if (!list.contains("pictures")) {
+        QSqlQuery query(mDatabase);
+        query.exec(QString("CREATE TABLE pictures")
+                   + " (id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                   + "album_id INTEGER, "
+                   + "url TEXT)");
+    }
 }
