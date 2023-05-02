a71991540e85876274cc931468decb0d4938e0b6
meteistar <gargamel_1987@hotmail.com>
2023-05-02 17:46:27 +0300

    Added addPictureInAlbum func implemantation

--- Gallery-Core/picturedao.cpp
+++ Gallery-Core/picturedao.cpp
@@ -3,6 +3,9 @@
 #include <QStringList>
 #include <QSqlDatabase>
 #include <QSqlQuery>
+#include <QVariant>
+
+#include "DatabaseManager.h"
 
 PictureDao::PictureDao(QSqlDatabase& database) :
     mDatabase(database)
@@ -21,4 +24,21 @@ void PictureDao::init() const
                    + "album_id INTEGER, "
                    + "url TEXT)");
     }
+}
+
+void PictureDao::addPictureInAlbum(int albumId, Picture &picture) const
+{
+    QSqlQuery query(mDatabase);
+        query.prepare(QString("INSERT INTO pictures")
+            + " (album_id, url)"
+            + " VALUES ("
+            + ":album_id, "
+            + ":url"
+            + ")");
+        query.bindValue(":album_id", albumId);
+        query.bindValue(":url", picture.fileUrl());
+        query.exec();
+        DatabaseManager::debugQuery(query);
+        picture.setId(query.lastInsertId().toInt());
+        picture.setAlbumId(albumId);
 }
